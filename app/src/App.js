8b41f4d55395df0bfa3c3dc28f1a28bb2e2fcc47
rock frederic <rockfrderic@proton.me>
2025-05-30 18:58:37 -0400

    new file:   app/ package.json 	new file:   app/public/favicon.ico 	new file:   app/public/index.html 	new file:   app/src/App.js 	new file:   app/src/index.js

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hello, React + PostgreSQL Dev!</h1>
+      <p>Edit your code via File Browser (http://localhost:8080) and see hot reload at http://localhost:3000.</p>
+    </div>
+  );
+}
+
+export default App;
